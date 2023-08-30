a9be27494039fff2d00bd44c2793e85bb2d479ba
stevenlittnyu <75270947+stevenlittnyu@users.noreply.github.com>
2023-08-30 14:54:58 -0400

    set examples to use INPUT

--- examples/multi_button_array_example/multi_button_array_example.ino
+++ examples/multi_button_array_example/multi_button_array_example.ino
@@ -3,10 +3,10 @@
 // the first argument is pin number, 
 // the second is the number which the event callback receives, 
 // the third is either INPUT or INPUT_PULLUP (this one is optional and is INPUT by default)
-StevesAwesomeButton button1(2, 0, INPUT_PULLUP);
-StevesAwesomeButton button2(3, 1, INPUT_PULLUP);
-StevesAwesomeButton button3(4, 2, INPUT_PULLUP);
-StevesAwesomeButton button4(5, 3, INPUT_PULLUP);
+StevesAwesomeButton button1(2, 0, INPUT);
+StevesAwesomeButton button2(3, 1, INPUT);
+StevesAwesomeButton button3(4, 2, INPUT);
+StevesAwesomeButton button4(5, 3, INPUT);
 
 StevesAwesomeButton* buttons[4] = { &button1, &button2, &button3, &button4 };
 
